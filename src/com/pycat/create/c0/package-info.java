6567f99267962468f0a598ad7df5491fe1353008
chengguibao <pythoncat.cheng@gometech.com.cn>
2018-06-08 13:54:16 +0800

    静态工厂方法前奏-- 不使用工厂方法的代码雏形

---
+++ src/com/pycat/create/c0/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by cat on 2018/6/8.
+ * 六个创建型模式: 简单工厂模式-Simple Factory Pattern (1/6)
+ */
+package com.pycat.create.c0;
---
+++ src/com/pycat/create/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by cat on 2018/6/8.
+ * 六个创建型模式
+ */
+package com.pycat.create;
